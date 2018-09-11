fe8abf2e45891ee906f90e83c3c610584f73f8da
Matthias Bussonnier <bussonniermatthias@gmail.com>
2018-09-11 23:16:19 +0200

    force-reset

--- sc.sh
+++ sc.sh
@@ -1,6 +1,6 @@
 git fetch origin
 git reset --hard origin/master
-git push origin master:1.x
+git push origin master:1.x --force
 echo ' - '$(date) >> readme.md
 git commit -am $(date "+%H:%M")
 git push github master:$(date "+%H-%M")
